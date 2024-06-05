1e191efbb83dd85d3d6964c69e177f848c047735
Capi-nemoo <79350389+Capi-nemoo@users.noreply.github.com>
2024-06-05 15:41:53 -0500

    # Source code for all devices

---
+++ src/mega2560/comments.cpp
@@ -0,0 +1,11 @@
+/*#include <TFT_HX8357.h>
+#include <User_Setup.h>
+#include <Wire.h>
+TFT_HX8357 tft = TFT_HX8357(); // Invoke custom library*/
+
+//variable selection layer
+///void receiveEvent(int howMany);
+////void selection_Layer();
+///void layer1();
+/////void layer2();
+///void layer3();
