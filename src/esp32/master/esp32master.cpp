21a4b9252f8b6d5b0ab32399645fb359f063a92e
Capi-nemoo <79350389+Capi-nemoo@users.noreply.github.com>
2024-06-06 15:23:11 -0500

    Revert "config.h, comments.cpp, main.cpp files"
    This reverts commit 7f3e93cfec492594b97bcdeacbd09a9a29fb3daa.

--- src/esp32/master/esp32master.cpp
+++ src/esp32/master/esp32master.cpp
@@ -1,5 +1,77 @@
-#include <config.h>
-#include <html.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <WebServer.h>
+#include <WiFi.h> 
+#include "htmlCity.h"  //CAMBIAR A .H ARCHI OJO IMPOPRTANTEEE
+
+//Network credentials
+const char* ssid = "Renaplex";
+const char* password = "1234567890";
+
+//variable de cehckcity cada 5segundos
+unsigned long tiempoAnterior = 0; // Almacena la última vez que se ejecutó la acción
+const unsigned long intervalo = 5000; // Intervalo deseado de 5 segundos
+
+const int arduino1 = 0x02;  // Ejemplo de dirección del esclavo 1
+const int espslave = 0x03;  // Ejemplo de dirección del esclavo 2
+const int mega2560 = 0x04;  // Ejemplo de dirección del esclavo 1
+
+/**
+ * @struct DatosClimaticos
+ * @brief Estructura para almacenar los datos climáticos recibidos de la API.
+ *
+ * Almacena temperatura actual, sensación térmica, temperaturas mínima y máxima, humedad, país y condición climática.
+ */
+struct CityData {
+    double temp;                ///< Temperatura actual.
+    double feels_like;          ///< Sensación térmica.
+    double temp_min;            ///< Temperatura mínima.
+    double temp_max;            ///< Temperatura máxima.
+    double humidity;            ///< Humedad.
+    String country;             ///< País.
+    String clima_condicion;     ///< Condición climática.
+};
+
+CityData cityData;      
+
+bool isDifferent = false;
+String checklastCity = "xd";
+String cityName = "xd";
+// Set web server port number to 80
+WebServer server(90);
+//definicion de data variable a manda al arduino mega
+
+// Variable to store the HTTP request
+String header;
+String climaFinal = "";
+
+int sizeCityData;
+void sendcityName();
+//void bytesOf();
+//void i2cUNO();
+//void i2cAPItodolist();
+void checkCity();
+void requestDataFromSlave(int slaveAddress, int count);
+void handleDataRequest();
+void waitForData();
+//void readbuffer();
+//void sendCasetomega();
+//void i2cUNO();
+//void i2cAPItodolist();
+void receive_citydataSize();
+void recieve_cityData();
+void printCityData(const CityData& data);
+void reenviar_cityData(const CityData& data);
+void requestDataFromSlave(int slaveAddress, int count);
+void handleDataRequest();
+void processReceivedByte(byte receivedByte);
+void waitForData();
+void checkCity();
+void i2cUNO();
+void i2cAPItodolist();
+void readbuffer();
+void sendCasetomega();
 
 void setup(){
   Serial.begin(115200);
@@ -69,6 +141,8 @@ void receive_citydataSize() {
     }
 }
 
+
+  //recibir}
 void recieve_cityData(){
   Wire.requestFrom(2, sizeCityData); // Solicita la estructura completa.
   Serial.print(sizeCityData);
@@ -119,6 +193,7 @@ void printCityData(const CityData& data) {
   Serial.println(data.clima_condicion);
 }
 
+
 void reenviar_cityData(const CityData& data) {
   // Reenviar los datos al mega2560 con dirección 4
   ///sendCasetomega(); //manda el case al megalocoooo
@@ -150,7 +225,8 @@ void reenviar_cityData(const CityData& data) {
   }
 }
 
-void requestDataFromSlave(int slaveAddress, int count) { // Función para leer la cantidad de bytes a recibir y realizar el request
+// Función para leer la cantidad de bytes a recibir y realizar el request
+void requestDataFromSlave(int slaveAddress, int count) {
     if (count > 0) {
         Wire.requestFrom(slaveAddress, count);
         while (Wire.available()) {
@@ -164,7 +240,8 @@ void requestDataFromSlave(int slaveAddress, int count) { // Función para leer l
     }
 }
 
-void handleDataRequest() {// Función que maneja la lógica de interpretación del primer byte y los bytes de tamaño
+// Función que maneja la lógica de interpretación del primer byte y los bytes de tamaño
+void handleDataRequest() {
   
     if (Wire.available() > 0) {  // Asegurarse de que hay al menos 3 bytes disponibles
         byte identifier = Wire.read();  // Primer byte: identificador del tipo y esclavo
@@ -196,7 +273,37 @@ void handleDataRequest() {// Función que maneja la lógica de interpretación d
         waitForData();
     }
 }
+/*
+// Función para procesar el byte recibido
+void processReceivedByte(byte receivedByte) {
 
+    // Checar el valor del byte con cada "if"
+    if (receivedByte == 1) {
+        Serial.println("......= 1 (transmisión del Arduino UNO / temperatura de los módulos)");
+        // Código de transmisión del Arduino UNO
+
+    } 
+    else if (receivedByte == 2) {
+        Serial.println("......= 2 (transmisión del ESP (esclavo) - Datos clima del API)");
+        // Código de transmisión del ESP (esclavo)
+  
+    }
+    else if (receivedByte == 3) {
+        Serial.println("......= 3 (transmisión de este mismo ESP (maestro) al Mega 2560)");
+        // Código de transmisión de este mismo ESP (maestro)
+
+    } 
+    else if (receivedByte == 4) {
+        Serial.println("......= 4 (transmisión del ESP32 (esclavo) - Datos de la API de Microsoft)");
+        // Código de transmisión del ESP32 (esclavo)
+
+    } else {
+        Serial.println("Ninguna opción fue seleccionada. Resetear el código o manejar como error.");
+    }
+    Serial.print("El tamaño del mensaje es de : ");
+    Serial.println(" bytes");
+}
+*/
 void waitForData() {
     Serial.println("Esperando a recibir un byte...");
     while (Wire.available() == 0) { // Mientras no haya bytes disponibles
@@ -230,3 +337,50 @@ void checkCity() {
   }
 }
 
+
+/*
+void i2cUNO() {
+    //tiene que recibir primero los datos del arduino UNO para mandarlos al mega2560
+  Wire.requestFrom(3, 10);
+  Serial.println("Se envia peticion al ArduinoUNO de 10bytes");
+  String datosUno;
+  char c = Wire.read();//se lee el primer byte
+  datosUno += c; // Agrega cada byte a la cadena
+  Serial.print("comienza ahora a reenviar los datos al mega2560");
+  Serial.print(datosUno);
+  Wire.beginTransmission(4); //esclavo con dirección 2
+  
+  for (int i = 0; i < 10; i++) {
+      Wire.write(datosUno[i]);
+  }
+}
+*/
+/*
+void i2cAPItodolist() {
+  //recibir los datos del espAPI y mandarlos al MEGA250
+}
+*/
+
+/*
+void readbuffer() {
+  //leer el buffer de el arduino UNO modulos fisicos
+  //leer el buffer de el ESP(esclavo) de la API de microsoft
+  //leer el buffer de el ESP(slave) de la API de openweather
+
+} 
+*/
+/*
+void sendCasetomega() {
+  // Send a byte to slave(4)
+  Wire.beginTransmission(4); // Start communication with slave at address 4
+  Wire.write(1); // Send a byte with value 1
+  byte error = Wire.endTransmission(); // End transmission and check for errors
+
+  if (error == 0) {
+    Serial.println("Byte sent successfully.");
+  } else {
+    Serial.print("Transmission error: ");
+    Serial.println(error);
+  }
+}
+*/
--- src/esp32/slave/esp32slave.cpp
+++ src/esp32/slave/esp32slave.cpp
@@ -1,4 +1,51 @@
-#include <config.h>
+#include <Wire.h>               ///< Librería para la comunicación I2C.
+#include <WiFi.h>               ///< Librería para la conexión WiFi.
+#include <HTTPClient.h>         ///< Librería para realizar solicitudes HTTP.
+#include <ArduinoJson.h>        ///< Librería para el manejo de JSON.
+#include <ArduinoJson.hpp>      ///< Librería para el manejo de JSON.
+
+// Variables globales para almacenar las credenciales de la red WiFi.
+int howMany;                    ///< Variable para almacenar la cantidad de bytes recibidos.
+const char* ssid = "18662675";  ///< Nombre de la red WiFi.
+const char* password = "Aluna18662675*"; ///< Contraseña de la red WiFi.
+
+// Variables para almacenar la primera parte de los datos recibidos y coordenadas geográficas.
+String firstBytex;     ///< Variable para almacenar el primer byte recibido.
+double latitude;                ///< Variable para almacenar la latitud geográfica.
+double longitude;               ///< Variable para almacenar la longitud geográfica.
+
+bool firstCycle = true;
+
+
+/**
+ * @struct DatosClimaticos
+ * @brief Estructura para almacenar los datos climáticos recibidos de la API.
+ *
+ * Almacena temperatura actual, sensación térmica, temperaturas mínima y máxima, humedad, país y condición climática.
+ */
+struct DatosClimaticos {
+    double temp;                ///< Temperatura actual.
+    double feels_like;          ///< Sensación térmica.
+    double temp_min;            ///< Temperatura mínima.
+    double temp_max;            ///< Temperatura máxima.
+    double humidity;            ///< Humedad.
+    String country;             ///< País.
+    String clima_condicion;     ///< Condición climática.
+};
+
+DatosClimaticos clima;     
+
+int sizeOfData;                 ///< Tamaño total de los datos climáticos.
+int length;                     ///< Longitud de algún dato.
+int totalSize;                  ///< Tamaño total de algo más.
+
+
+
+void receiveEvent(int howMany);
+void climaCiudad(const String& ciudad, DatosClimaticos& clima);
+int calculateTotalSize(const DatosClimaticos& clima);
+void coordenadasCiudad(String ciudad);
+void requestEvent();
 
 /**
  * @brief Configuración inicial del microcontrolador.
